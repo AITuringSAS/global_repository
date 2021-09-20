89717a700a95837c4621f1861909bf6448b23953
Daniel Tobon <daniel.tobon@aituring.co>
2021-09-19 21:43:24 -0500

    Added eval_batch_size parameter

--- efficientdet_aituring/main.py
+++ efficientdet_aituring/main.py
@@ -82,14 +82,18 @@ def main():
     7. save frozen model 
     """
 
+    # Setting default arguments:
     parser = argparse_flags.ArgumentParser(
         description='Command Line Tools EfficientDet Interface.', fromfile_prefix_chars='@')
     parser.add_argument('--URL_DATASET', help='url from S3.')
     parser.add_argument('--BACKBONE_REF', help='backbone name e.g. efficientdet-d0')
     parser.add_argument('--NUM_EPOCHS', help='number of epochs. e.g 100')
-    parser.add_argument('--BATCH_SIZE', help='size of batch e.g. 6')
+    parser.add_argument('--TRAIN_BATCH_SIZE', help='size of batch for training e.g. 6')
+    parser.add_argument('--EVAL_BATCH_SIZE', help='size of batch for evaluation e.g. 6')
     parser.add_argument('--MODEL_CKPTS', help='path to save train model. (default: saved in workspace)')
-
+    parser.add_argument('--NUM_EXAMPLES_PER_EPOCH', help='number of examples for training an epoch')
+    
+    # Dictionary for managing paths
     paths = {
         'WORKSPACE': os.path.join(os.getcwd(), 'workspace'),
         'DATASET_DIR': os.path.join(os.getcwd(), 'workspace', 'dataset'),
@@ -99,6 +103,7 @@ def main():
         'BACKBONE_CKPT_DIR': os.path.join(os.getcwd(), 'workspace', 'ckpt')
     }
 
+    # Dictionary for managing files
     files = {
         'TFRECORD_SCRIPT': os.path.join(os.getcwd(), 'tfrmod', 'create_tfrecords.py'),
         'EFFICIENTDET_MAIN_SCRIPT': os.path.join(os.getcwd(), 'automl', 'efficientdet', 'main.py'),
@@ -107,6 +112,7 @@ def main():
         'DATASET_FILE': os.path.join(os.getcwd(), 'workspace', 'dataset', 'dataset.zip'),
     }
 
+    # Dictionary for backbone url managment
     backbone_url = {
         'efficientdet-d0': 'https://storage.googleapis.com/cloud-tpu-checkpoints/efficientdet/coco2/efficientdet-d0.tar.gz',
         'efficientdet-d1': 'https://storage.googleapis.com/cloud-tpu-checkpoints/efficientdet/coco2/efficientdet-d1.tar.gz',
@@ -114,6 +120,12 @@ def main():
         'efficientdet-d3': 'https://storage.googleapis.com/cloud-tpu-checkpoints/efficientdet/coco2/efficientdet-d3.tar.gz'
     }
 
+    # check if not arguments are given
+    # if not args, check if env parameters were given
+    if len(sys.argv) < 2 and not all([os.getenv('URL_DATASET'), os.getenv('BACKBONE_REF'), os.getenv('NUM_EPOCHS'), os.getenv('TRAIN_BATCH_SIZE'), os.getenv('MODEL_CKPTS')]):
+        parser.print_usage()
+        sys.exit(1)
+        
     # Setting default args to workspace
     flags_tfrecord.data_dir = paths['DATASET_DIR']
     flags_tfrecord.output_path = os.path.join(paths['TFRECORD_DIR'], 'tfrecord')
@@ -122,32 +134,56 @@ def main():
     flags_efficientdet.hparams = files['HPARAMS_YAML']
 
     flags_freeze.path_yaml = files['HPARAMS_YAML']
-
-    if len(sys.argv) < 2 and not (os.environ['URL_DATASET'] or os.environ['BACKBONE_REF'] or os.environ['NUM_EPOCHS'] or os.environ['BATCH_SIZE'] or os.environ['MODEL_CKPTS']):
-        parser.print_usage()
-        sys.exit(1)
         
-
-    # Setting args by command line
     # Default args will be overridden if provided by command line
     args = parser.parse_args()
     
     # catch environment parameters if not detected by command line or config.file
     if not args.URL_DATASET:
-        args.URL_DATASET = os.environ['URL_DATASET']
+        if "URL_DATASET" in os.environ:
+            args.URL_DATASET = os.getenv('URL_DATASET')
+        else:
+            parser.print_usage()
+            sys.exit(1)
     if not args.BACKBONE_REF:
-        args.BACKBONE_REF = os.environ['BACKBONE_REF']
+        if "BACKBONE_REF" in os.environ:
+            args.BACKBONE_REF = os.getenv('BACKBONE_REF')
+        else:
+            parser.print_usage()
+            sys.exit(1)
     if not args.NUM_EPOCHS:
-        args.NUM_EPOCHS = int(os.environ['NUM_EPOCHS'])
-    if not args.BATCH_SIZE:
-        args.BATCH_SIZE = int(os.environ['BATCH_SIZE'])
+        if "NUM_EPOCHS" in os.environ:
+            args.NUM_EPOCHS = int(os.getenv('NUM_EPOCHS'))
+        else:
+            parser.print_usage()
+            sys.exit(1)
+    if not args.TRAIN_BATCH_SIZE:
+        if "TRAIN_BATCH_SIZE" in os.environ:
+            args.TRAIN_BATCH_SIZE = int(os.getenv('TRAIN_BATCH_SIZE'))
+        else:
+            parser.print_usage()
+            sys.exit(1)
+    if not args.EVAL_BATCH_SIZE:
+        if "EVAL_BATCH_SIZE" in os.environ:
+            args.EVAL_BATCH_SIZE = int(os.getenv('EVAL_BATCH_SIZE'))
+        else:
+            parser.print_usage()
+            sys.exit(1)
     if not args.MODEL_CKPTS:
         if "MODEL_CKPTS" in os.environ:
-            args.MODEL_CKPTS = os.environ['MODEL_CKPTS']
+            args.MODEL_CKPTS = os.getenv('MODEL_CKPTS')
         else:
             args.MODEL_CKPTS = paths['MODEL_OUTPUT_DIR']
+            sys.exit(1)
+    if not args.NUM_EXAMPLES_PER_EPOCH:
+        if "NUM_EXAMPLES_PER_EPOCH" in os.environ:
+            args.NUM_EXAMPLES_PER_EPOCH = int(os.getenv('NUM_EXAMPLES_PER_EPOCH'))
+        else:
+            parser.print_usage()
+            sys.exit(1)
 
 
+    # Create workspace directories
     try:
         os.mkdir(paths['WORKSPACE'])
         os.mkdir(paths['DATASET_DIR'])
@@ -159,21 +195,23 @@ def main():
     except OSError:
         pass
 
+    # Update flags provided by command line
     flags_efficientdet.model_name = args.BACKBONE_REF
     flags_efficientdet.backbone_ckpt = os.path.join(paths['BACKBONE_CKPT_DIR'], args.BACKBONE_REF)
     flags_efficientdet.num_epochs = args.NUM_EPOCHS
-    flags_efficientdet.train_batch_size = args.BATCH_SIZE
+    flags_efficientdet.train_batch_size = args.TRAIN_BATCH_SIZE
+    flags_efficientdet.eval_batch_size = args.EVAL_BATCH_SIZE
+    flags_efficientdet.num_examples_per_epoch = args.NUM_EXAMPLES_PER_EPOCH
 
-
+    # Get folder name for results
     path_ckpt = os.path.basename(os.path.normpath(args.MODEL_CKPTS))
 
-    # path_ckpt = os.listdir(paths['MODEL_OUTPUT_DIR'])
-    # flags_freeze.path_ckpt = os.path.join(paths['MODEL_OUTPUT_DIR'], path_ckpt[0])
+    # Set output dir
     flags_freeze.path_ckpt = os.path.join(paths['MODEL_OUTPUT_DIR'], path_ckpt)
     flags_freeze.model_name_ = args.BACKBONE_REF
     if not flags_freeze.path_output:
         flags_freeze.path_output = os.path.join(paths['FROZEN_MODEL_DIR'], path_ckpt + '_freeze')
- 
+
 
     # Download dataset from S3
     download_and_uncompress_dataset(args.URL_DATASET, paths['DATASET_DIR'], files['DATASET_FILE'])
